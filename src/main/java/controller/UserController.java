3674714b23d64f3fe08ae51d76f85bdf1924a82f
darkkinght07 <ion.ursu.dev@gmail.com>
2023-04-03 21:56:05 +0200

    upgraded to latest spring boot.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,15 +1,19 @@
 package controller;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World!";
+    @GetMapping("/home")
+    public ResponseEntity home() {
+        return new ResponseEntity("Hello World!", HttpStatus.OK) ;
     }
 
     public static void main(String[] args) {
